3aaee88f81c9fa7dea80a0941433e2097b9747c1
khizar85 <khizarkhan8585@gmail.com>
2026-06-25 16:24:07 +0500

    updated auth api URL to vercel backend

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    // baseURL: "http://localhost:3000",
+    baseURL: "https://cv-analyzer-backend-neon.vercel.app",
     withCredentials: true
 })
 
